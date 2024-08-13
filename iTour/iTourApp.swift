5e7ec91e05a4b0cc59b0696059f7235b3be44d7e
denysooz <den.darevskiy@mail.ru>
2024-08-13 11:48:13 -0400

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Denis Dareuskiy on 13.08.24.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
