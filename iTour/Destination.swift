eed6e472e9a76a2258f7681b3c15764082a17b35
denysooz <den.darevskiy@mail.ru>
2024-08-13 20:03:35 -0400

    Working with relationships

--- iTour/Destination.swift
+++ iTour/Destination.swift
@@ -14,6 +14,7 @@ class Destination {
     var details: String
     var date: Date
     var priotiy: Int
+    @Relationship(deleteRule: .cascade) var sights = [Sight]()
     
     init(name: String = "", details: String = "", date: Date = .now, priotiy: Int=2) {
         self.name = name
